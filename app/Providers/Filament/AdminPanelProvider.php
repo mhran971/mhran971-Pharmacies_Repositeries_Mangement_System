e2e442bcfa5d9d8353e349afdffe4742180e07ba
Mahran_AD <mhranabwdqt971@gmail.com>
2025-05-23 13:53:48 +0300

    some update at  Filament Panel Builder for repoOwner and admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,9 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->passwordReset()
+            ->profile()
+
             ->colors([
                 'primary' => Color::Amber,
             ])
