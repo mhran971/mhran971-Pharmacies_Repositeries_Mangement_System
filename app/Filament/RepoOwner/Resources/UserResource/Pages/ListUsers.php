8c84a2a356117ec507d474b39908fe7029701b18
Mahran_AD <mhranabwdqt971@gmail.com>
2025-05-24 21:08:57 +0300

    make user resource in filament

---
+++ app/Filament/RepoOwner/Resources/UserResource/Pages/ListUsers.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\RepoOwner\Resources\UserResource\Pages;
+
+use App\Filament\RepoOwner\Resources\UserResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
