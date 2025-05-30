239bd151206e501183d6087c2f0e743a3eeabd56
Mahran_AD <mhranabwdqt971@gmail.com>
2025-05-30 15:03:15 +0300

    make ( repo_owner - employee - pharmacy_owner - pharmacist ) Requests

---
+++ app/Http/Requests/Repository/Auth/Repo_OwnerRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Repository\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Repo_OwnerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:20|unique:users,phone_number',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'repository_name' => 'required|string|max:255',
+            'repository_phone' => 'required|string|max:20',
+            'repository_address' => 'required|string|max:255',
+        ];
+    }
+
+}
