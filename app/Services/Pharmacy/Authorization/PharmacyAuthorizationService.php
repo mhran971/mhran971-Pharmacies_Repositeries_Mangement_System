f87a82bf13b55d92050374edbb87a708fc0ade94
Mahran_AD <mhranabwdqt971@gmail.com>
2025-06-04 01:13:31 +0300

    test commit

--- app/Services/Pharmacy/Authorization/PharmacyAuthorizationService.php
+++ app/Services/Pharmacy/Authorization/PharmacyAuthorizationService.php
@@ -9,11 +9,10 @@ class PharmacyAuthorizationService
     public function get_permissions($lang): \Illuminate\Support\Collection
     {
         if ($lang == 'ar') {
-            return $ar_data = Permission::all()->pluck('name_ar');
+            $data = $ar_data = Permission::all()->pluck('name_ar');
         }
-        if ($lang == 'en') {
-            return $en_data = Permission::all()->pluck('name_en');
-        }
-
+        if ($lang == 'en')
+            $data = $en_data = Permission::all()->pluck('name_en');
+        return $data;
     }
 }
