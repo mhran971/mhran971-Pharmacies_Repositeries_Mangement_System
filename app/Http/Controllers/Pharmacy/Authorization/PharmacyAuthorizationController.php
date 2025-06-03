705dbc1cca706db39963adb67e7560d9f9dd9f89
Mahran_AD <mhranabwdqt971@gmail.com>
2025-06-04 01:11:27 +0300

    Added get_all_users and assign_permissions_user methods to PharmacyAuthorizationController and RepoAuthorizationController.

--- app/Http/Controllers/Pharmacy/Authorization/PharmacyAuthorizationController.php
+++ app/Http/Controllers/Pharmacy/Authorization/PharmacyAuthorizationController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Pharmacy\Authorization;
 
 use App\Http\Controllers\BaseController;
+use App\Http\Requests\Pharmacy\Authorization\Assign_PermissionRequest;
+use App\Models\User;
 use App\Services\Pharmacy\Authorization\PharmacyAuthorizationService;
+use Illuminate\Http\Request;
 
 class PharmacyAuthorizationController extends BaseController
 {
@@ -13,7 +16,14 @@ class PharmacyAuthorizationController extends BaseController
     {
         $this->pharmacyAuthorizationService = $pharmacyAuthorizationService;
     }
-
+    public function get_all_users()
+    {
+        $data = $this->pharmacyAuthorizationService->get_users();
+        if ($data)
+            return $this->sendResponse($data,);
+        else
+            $this->sendError('Something got wrong');
+    }
     public function get_all_permissions($lang)
     {
         $data = $this->pharmacyAuthorizationService->get_permissions($lang);
@@ -21,6 +31,16 @@ class PharmacyAuthorizationController extends BaseController
             return $this->sendResponse($data,);
         else
             $this->sendError('Something got wrong');
+    }
+
+    public function assign_permissions_user($user_id, Assign_PermissionRequest $request): \Illuminate\Http\JsonResponse
+    {
+        $user = User::findorFail($user_id);
+        $data = $this->pharmacyAuthorizationService->assign_permissions($user_id, $request);
+        return $this->sendResponse(
+            $data,
+            "permissions " . implode(',', $data['permissions_id']) . " assigned successfully to {$user->name}"
+        );
     }
 
 }
